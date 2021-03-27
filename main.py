aad909276756fbde37fd9c2be09ca282cb9c4afb
Fidio-lp2 <qdatian9@gmail.com>
2021-03-27 21:27:33 +0900

    一応完成　エイリアス設定してパス通してコマンドみたいに利用してクレメンス

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-#!/usr/bin/python3
 import requests
 import json
 import sys
 
 def main():
 
-    with open('data.json', mode='rt', encoding='utf-8') as file:
+    with open('Term_Translation_data.json', mode='rt', encoding='utf-8') as file:
         data = json.load(file)
 
         text = sys.argv[1]
@@ -24,19 +23,23 @@ def main():
         source = target
         target = tmp
 
+        reinsert(data, source, target)
+
         pass
 
     elif sys.argv[1] == 'set':
 
         if sys.argv[2] == '-s':
-            source = sys.argv[3]
+            source = data['languages'][sys.argv[3]]
 
         elif sys.argv[2] == '-t':
-            target = sys.argv[3]
+            target = data['languages'][sys.argv[3]]
 
         else:
-            source = sys.argv[2]
-            target = sys.argv[3]
+            source = data['languages'][sys.argv[2]]
+            target = data['languages'][sys.argv[3]]
+
+        reinsert(data, source, target)
 
         pass
 
@@ -51,6 +54,8 @@ def reinsert(data, source, target):
     data['status']['source'] = source
     data['status']['target'] = target
 
+    with open('Term_Translation_data.json', 'w') as f:
+        json.dump(data, f, indent=4)
 
 if __name__ == '__main__':
     main()
